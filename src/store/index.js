a43ff1be8cab41a64c797bd492c080424fab772f
Alexei Kekin <tcore.ak@gmail.com>
2020-02-29 13:24:23 +0200

    Basic todo version

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,27 @@
 import { counterReducer } from '../modules/counter/reducer';
 import { todoReducer } from '../modules/todo/reducer';
+import {applyMiddleware} from 'redux';
 
 const combinedReducers = window.Redux.combineReducers({
   counterReducer,
   todoReducer,
 });
 
+const logger = store => next => action => {
+  console.log(JSON.stringify(action, null , 4));
+  next(action);
+};
+//
+// const delayActions = store => next => action => {
+//   console.log('Debounce middleware START');
+//   setTimeout(() => {
+//     next(action);
+//     console.log('Debounce middleware END');
+//   }, 3000);
+// };
+
 export const store = window.Redux.createStore(
   combinedReducers,
   undefined,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger),
 );
